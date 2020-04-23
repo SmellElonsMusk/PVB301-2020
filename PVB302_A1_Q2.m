f186cbee4a7d097239c849a614ab5d50fa142122
Waldo Fouche <waldofouche1@gmail.com>
2020-04-23 18:28:05 +1000

    Update PVB302_A1_Q2.m
    Added: Updated the Script to loop through the Multiplicity  and  Entropy Equations

--- PVB302_A1_Q2.m
+++ PVB302_A1_Q2.m
@@ -5,35 +5,43 @@
 clear; % Testing only
 
 % Question 2 - Coupled Einstein Solids
-% Paramaters:
-Na = 250;
+% Paramaters:  
+Na = 250; 
 Nb = 250;
 qtotal = Na + Nb;
-
-% Testing only
-qA = 1;
-qB = 499;
+kb = 1.3806 * 10^-23; 
 
 % Define Arrays:
-%qA = 0:1:Na;    %0 - Na incremnting by 1
-%qB = Nb:-1:0;   %Nb - 0 incremnting by 1
+qA = 0:1:qtotal;    %0 - Na incremnting by 1
+qB = qtotal:-1:0;   %Nb - 0 incremnting by 1
 
-% Turn the following into For Loops
+% Define Arrays with preallocated size for efficiency
+OmegaA= zeros(1,qtotal+1);
+OmegaB = zeros(1,qtotal+1);
+OmegaTotal = zeros(1,qtotal+1);
+Sa = zeros(1,qtotal+1);
+Sb = zeros(1,qtotal+1);
+Stotal = zeros(1,qtotal+1);
 
-% Omega Calculations:
+% Compute the Values and Insert into arrays
+n = 1;
+while n < qtotal+1
+    oA = omega(qA(n),Na);
+    oB = omega(qB(n),Nb);
+    
+    % Omega Calculations:
+    OmegaA(n) = oA;
+    OmegaB(n) = oB;
+    OmegaTotal(n) = OmegaA(n) * OmegaB(n);
+    
+    % Entropy Calculations:
+    Sa(n) = kb*log(OmegaA(n));
+    Sb(n) = kb*log(OmegaB(n));
+    Stotal(n) = Sa(n) + Sb(n);
+    
+    n = n + 1;
+end
 
-OmegaA = omega(qA,Na);
-OmegaB = omega(qB,Nb);
-OmegaTotal = OmegaA + OmegaB;
-
-% Entropy Calculations:
-
-kb = 1.3806 * 10^-23; 
-Sa = kb*log(OmegaA);
-Sb = kb*log(OmegaB);
-Stotal = Sa + Sb;
-
-% --------------------------------------------%
 %& Plotting %%
 
 % Multiplicty of Qa for a Qtotal of x
@@ -41,17 +49,17 @@ figure
 plot(qA, OmegaTotal);
 xlabel('qA');
 ylabel('Multiplicty');
-title('Multiplicty of Qa for a Qtotal of x');
+title(['Multiplicty of Qa for a Qtotal of ', num2str(qtotal)]);
 
 % Entropy in a System with a Qtotal of x
 figure
 plot(qA,Sa,qA,Sb,qA,Stotal);
 xlabel('qA');
 ylabel('Entropy (In units of Kb)');
-title('Entropy in a System with Qtotal of x');
+title(['Entropy in a System with Qtotal of ', num2str(qtotal)]);
 
 
-% Function Definitions 
+% Omega Function Definition
 function f = omega(q,N)
     n = q + N -1;
     k = N-1;
